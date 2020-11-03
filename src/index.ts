1d07d047d3be8edcf647a84abc0da12f56793481
Ian Sutherland <ian@iansutherland.ca>
2020-11-02 20:19:49 -0700

    Add custom errors and async methods

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
 import FlinksClient from './client';
 
+export { AuthorizeOptions, FlinksAuthorizeResponse, AuthorizeResponse } from './commands/authorize';
+
+export {
+  GetAccountsDetailOptions,
+  GetAccountsDetailAsyncOptions,
+  FlinksGetAccountsDetailResponse,
+  FlinksGetAccountsDetailAsyncResponse,
+  GetAccountsDetailResponse,
+  GetAccountsDetailAsyncResponse
+} from './commands/accounts-detail';
+
+export {
+  GetAccountsSummaryOptions,
+  FlinksGetAccountsSummaryResponse,
+  FlinksGetAccountsSummaryAsyncResponse,
+  GetAccountsSummaryResponse,
+  GetAccountsSummaryAsyncResponse
+} from './commands/accounts-summary';
+
+export {
+  GetStatementsOptions,
+  FlinksGetStatementsResponse,
+  FlinksGetStatementsAsyncResponse,
+  GetStatementsResponse,
+  GetStatementsAsyncResponse
+} from './commands/statements';
+
 export * from './lib/authenticity';
+
 export default FlinksClient;
